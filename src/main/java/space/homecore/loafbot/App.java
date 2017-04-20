e359c657c67ef97bfe4c567e8c18ac2f141755b0
Epowerj <epowerj@gmail.com>
2017-04-20 17:03:01 -0400

    Added empty maven project

---
+++ src/main/java/space/homecore/loafbot/App.java
@@ -0,0 +1,11 @@
+package space.homecore.loafbot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp target/loafbot-1.0-SNAPSHOT.jar space.homecore.loafbot.App
